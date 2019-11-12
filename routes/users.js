6bf0d49a06386ee9b89605b0c0d89f1d85225e3c
florent <florent@armor-capital.com>
2019-11-12 16:49:29 +0800

    Corrected the dashboard routes

--- routes/users.js
+++ routes/users.js
@@ -86,7 +86,7 @@ router.post('/register', (req, res) => {
 // Login Handle
 router.post('/login', (req, res, next) => {
     passport.authenticate('local', {
-        successRedirect: '/dashboard',
+        successRedirect: '/Dashboard',
         failureRedirect: '/users/login',
         failureFlash: true
     })(req, res, next);
